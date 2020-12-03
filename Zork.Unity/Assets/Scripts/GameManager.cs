cb5cede9fb24fc5482b277b2ab9719a7e8ef4453
adambrewer <55362205+adambrewer@users.noreply.github.com>
2020-12-02 23:55:10 -0500

    ZorkInUnity

---
+++ Zork.Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Zork;
+public class GameManager : MonoBehaviour
+{
+
+    void Awake()
+    {
+        TextAsset gameJsonAsset = Resources.Load<TextAsset>(ZorkGameFileAssetName);
+        Game.Start(gameJsonAsset.text);
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    [SerializeField]
+    private string ZorkGameFileAssetName = "Zork";
+}
